ce03dc50ed5097173e4b9fa657623a2b5db0affc
RuggedPOD <team@ruggedpod.qyshare.com>
2016-04-06 16:31:19 +0000

    Generate documentation website at f6f9ced

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -ex
+
+if [ "$TRAVIS_PULL_REQUEST" == "true" ]
+then
+  exit 0
+fi
+
+#if [ "$TRAVIS_BRANCH" != "master" ]
+#then
+#  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+#  exit 0
+#fi
+
+rev=$(git rev-parse --short HEAD)
+
+cd _book
+
+git init
+git config user.name "RuggedPOD"
+git config user.email "team@ruggedpod.qyshare.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/ruggedpod/ruggedpod-doc.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+touch .
+
+git add -A .
+git commit -m "Generate documentation website at ${rev}"
+git push -q upstream HEAD:gh-pages
